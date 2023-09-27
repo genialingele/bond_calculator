2d2453cca5afb433890bc7367146b09080d5b1c1
Genial Ingele <61932424+genialingele@users.noreply.github.com>
2023-09-27 16:08:19 +0000

    Remove Period as input Add costs on html page Make style changes such as vertical text height

--- beta_version/calculator_script.js
+++ beta_version/calculator_script.js
@@ -18,9 +18,34 @@
 var input_fields = [document.getElementById("purchase_price"), document.getElementById("deposit"), document.getElementById("interest_rate"), document.getElementById("number_of_years")]
 
 
-
 //A validation function to ensure that only numbers between 0 - 9 are entered
 function isNumber(evt) {
+      // The 'evt' parameter represents the event object passed to this function,
+    // typically an event like keypress or keydown.
+
+    // The following line determines the character code of the key pressed.
+    // It checks both 'which' and 'keyCode' properties, as these can vary
+    // depending on the browser being used.
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+
+    // Now, it checks whether the character code corresponds to a numeric key (0-9).
+    // The condition checks if the charCode is greater than 31 (to exclude control characters)
+    // and if it's not in the range of 48-57 (ASCII codes for 0 to 9).
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        // If the character code is not within the range of 0-9, the function returns false,
+        // indicating that the input should be rejected.
+        return false;
+    }
+
+    // If the character code is within the range of 0-9, the function returns true,
+    // allowing the input to be processed as a valid number.
+    return true;
+
+}
+
+
+//A validation function to ensure that DECIMAL numbers only entered
+function isDecimalNumber(evt) {
   var charCode = (evt.which) ? evt.which : evt.keyCode;
 
   // Check if a period has already been typed.
@@ -37,6 +62,7 @@ function isNumber(evt) {
       return false;
   }
 }
+
 
 //Use the enter key to trigger calculation
 for (let i = 0 ; i < input_fields.length ; i++){
@@ -57,14 +83,7 @@ for (let i = 0 ; i < input_fields.length ; i++){
   }
 }
 
-/* //read values entered by the user on the input fields, convert them to local variables
-const purchase_price_input =document.getElementById("purchase_price").value;
-const interest_rate_input = document.getElementById("interest_rate").value;
-const years_input = document.getElementById("number_of_years").value;
-const deposit_input = document.getElementById("deposit").value;
-const submitButton = document.getElementById("submit_btn");   */
-
-
+//Bond calculator algorithm. It takes in 3 parameters : price, interest, years, deposit amount (if available)
 function bondCalculator(purchase_price, interest_rate, years, deposit){
     //the mathematical formula is A = P * ( r * (1+r)^n ) / ( (1+r)^n - 1 )
     /*
